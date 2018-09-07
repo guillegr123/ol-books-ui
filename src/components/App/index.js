9cf9e5af9594be70029d164eae29188eb8077db9
Guillermo Gutierrez <guillegr123@gmail.com>
2018-09-06 22:48:33 -0600

    Added search box + missing Semantic UI CSS styles.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
+import { Input } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      searchText: ''
+    }
+  }
+
+  onSearchTextChanged = e => {
+    var searchText = e.target.value;
+    this.setState({ searchText, isLoading: searchText.trim() !== '' });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Biblioteca abierta</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <body className="App-body">
+          <Input
+            loading={ this.state.isLoading }
+            value={ this.state.searchText }
+            onChange={ this.onSearchTextChanged }
+            iconPosition="right"
+            placeholder="Buscar..." />
+            <br />
+            {
+              this.state.isLoading
+              &&
+              <p>
+                Buscando &quot;{ this.state.searchText }&quot;...
+              </p>
+            }
+        </body>
       </div>
     );
   }
